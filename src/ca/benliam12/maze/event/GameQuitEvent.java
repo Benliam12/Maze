88808f4b00936ad31e281b0fc312d65eed14be32
Benliam12 <s100live.ca@live.ca>
2015-04-12 08:15:43 -0400

    Implementation of own Events

--- src/ca/benliam12/maze/event/GameQuitEvent.java
+++ src/ca/benliam12/maze/event/GameQuitEvent.java
@@ -11,10 +11,14 @@ public class GameQuitEvent extends Event{
 	private Player player;
 	private Game game;
 	
-	@Override
+	private static final HandlerList handlers = new HandlerList();
+	 
 	public HandlerList getHandlers() {
-		// TODO Auto-generated method stub
-		return null;
+	    return handlers;
+	}
+	 
+	public static HandlerList getHandlerList() {
+	    return handlers;
 	}
 
 	public GameQuitEvent(Game game, Player player)
